08ba4928dfadc65e033943598667425b94c89f1e
Marc Meszaros <marc@snapable.com>
2012-11-16 00:44:19 -0500

    Camera takes photos

--- app/src/ca/hashbrown/snapable/EventPhotoList.java
+++ app/src/ca/hashbrown/snapable/EventPhotoList.java
@@ -1,19 +1,36 @@
 package ca.hashbrown.snapable;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import ca.hashbrown.snapable.fragments.PhotoListFragment;
 
 import com.snapable.api.models.Event;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class EventPhotoList extends FragmentActivity {
+public class EventPhotoList extends FragmentActivity implements OnClickListener {
+	
+	private static final String TAG = "EventPhotoList";
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.activity_photo_list);
 
+    	// add click listener
+    	findViewById(R.id.activity_photo_list__photo_button).setOnClickListener(this);
+    	
+    	// get the extra bundle data for the fragment
     	Bundle bundle = getIntent().getExtras();
 		Event event = bundle.getParcelable("event");
 		
@@ -23,5 +40,64 @@ public class EventPhotoList extends FragmentActivity {
  			photoListFragment.setEvent(event);
  		}
     }
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.activity_photo_list__photo_button:
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			
+			Uri imageUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+			
+			startActivityForResult(intent, 0);
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	
+	/** Create a file Uri for saving an image or video */
+	private static Uri getOutputMediaFileUri(int type) {
+	      return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type) {
+	    // To be safe, you should check that the SDCard is mounted
+	    // using Environment.getExternalStorageState() before doing this.
+
+	    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Snapable");
+	    // This location works best if you want the created images to be shared
+	    // between applications and persist after your app has been uninstalled.
+
+	    // Create the storage directory if it does not exist
+	    if (! mediaStorageDir.exists()){
+	        if (! mediaStorageDir.mkdirs()){
+	            Log.d(TAG, "failed to create directory");
+	            return null;
+	        }
+	        Log.d(TAG, "created storage directory");
+	    }
+
+	    // Create a media file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    File mediaFile;
+	    if (type == MEDIA_TYPE_IMAGE){
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+	    } else {
+	        return null;
+	    }
+
+	    return mediaFile;
+	}
 
 }
