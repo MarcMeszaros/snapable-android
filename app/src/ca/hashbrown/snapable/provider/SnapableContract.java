a2144b6a420fea2af98ecdba10a489f629a4a5ba
Marc Meszaros <marc@snapable.com>
2012-12-30 11:54:02 -0500

    Cleaned up some of the code

--- app/src/ca/hashbrown/snapable/provider/SnapableContract.java
+++ app/src/ca/hashbrown/snapable/provider/SnapableContract.java
@@ -15,29 +15,29 @@ public class SnapableContract {
 	/*
 	public static final class Account {
 		public static final String RESOURCE_NAME = "account";
-		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME );
+		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME);
 	}
 	*/
 	
 	public static final class Event {
 		public static final String RESOURCE_NAME = "event";
-		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME );
+		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME);
 	}
 	
 	public static final class Photo {
 		public static final String RESOURCE_NAME = "photo";
-		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME );
+		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME);
 	}
 
 	public static final class Guest {
 		public static final String RESOURCE_NAME = "guest";
-		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME );
+		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME);
 	}
 
 	/*
 	public static final class User {
 		public static final String RESOURCE_NAME = "user";
-		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME );
+		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + RESOURCE_NAME);
 	}
 	*/
 }
--- app/src/com/snapable/api/SnapBitmapSerializer.java
+++ app/src/com/snapable/api/SnapBitmapSerializer.java
@@ -1,11 +1,10 @@
 package com.snapable.api;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
 
-import org.codegist.common.io.IOs;
 import org.codegist.crest.serializer.Serializer;
 
 import android.graphics.Bitmap;
@@ -15,16 +14,11 @@ public class SnapBitmapSerializer implements Serializer<Bitmap> {
 
 	private static final String TAG = "SnapBitmapSerializer";
 
-	public void serialize(Bitmap value, Charset charset, OutputStream out) throws Exception {
+	public void serialize(Bitmap value, Charset charset, OutputStream out) throws IOException {
 		Log.d(TAG, value.getWidth() + "x" + value.getHeight());
-		byte[] imageData = null;
-    	ByteArrayOutputStream baos = new ByteArrayOutputStream();  
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();  
         value.compress(Bitmap.CompressFormat.JPEG, 50, baos);
-        imageData = baos.toByteArray();
-        
-        Log.d(TAG, "length: "+imageData.length);
-        
-        out.write(imageData);
+        out.write(baos.toByteArray());
 	}
 
 }
