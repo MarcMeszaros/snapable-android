57eb6484e6960a3bce55278d54bf1e0358bd1daf
Marc Meszaros <marc@snapable.com>
2013-01-17 15:14:30 -0500

    Setup Unit Testing

---
+++ test/src/ca/hashbrown/snapable/test/AllTests.java
@@ -0,0 +1,16 @@
+package ca.hashbrown.snapable.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests extends TestSuite {
+
+    public static Test suite() {
+
+            TestSuite suite = new TestSuite();
+            //suite.addTestSuite(CameraActivityTest.class);
+
+            return suite;
+    }
+
+}
