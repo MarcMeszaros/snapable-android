6540f6b44cc68f01d2d2c20284fe2b7eb6e4bbf8
Marc Meszaros <marc.meszaros@gmail.com>
2013-01-28 23:59:56 -0500

    Refactored code so it uses ABS

--- app/src/ca/hashbrown/snapable/EventList.java
+++ app/src/ca/hashbrown/snapable/EventList.java
@@ -7,48 +7,23 @@ import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.widget.SearchView;
 import com.actionbarsherlock.widget.SearchView.OnQueryTextListener;
 
+import ca.hashbrown.snapable.R;
 import ca.hashbrown.snapable.fragments.EventListFragment;
 
 import android.os.Bundle;
 
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.SearchViewCompat;
 import android.util.Log;
 
 public class EventList extends SherlockFragmentActivity implements OnQueryTextListener {
 
 	private static final String TAG = "EventList";
 
-	public SearchViewCompat.OnQueryTextListenerCompat searchListener = null;
-	//ActionBar actionBar;
-
 	@Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.activity_event_list);
-    	//actionBar = getSupportActionBar();
-
-    	// create the fragments
-    	EventListFragment eventListFragment = new EventListFragment();
-
-    	// add the fragments
-    	FragmentTransaction transaction = getCompatFragmentManager().beginTransaction();
-    	transaction.add(R.id.activity_event_list__fragment_event_list, eventListFragment, "EVENT_LIST_FRAGMENT");
-    	transaction.commit();
-	}
-
-	// used to pass back the search field listener back to the search view in the fragment
-	//public OnQueryTextListenerCompat getOnQueryTextListenerCompat() {
-	//	return searchListener;
-	//}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	private FragmentManager getCompatFragmentManager() {
-			return getSupportFragmentManager();
 	}
 
 	@Override
@@ -73,7 +48,7 @@ public class EventList extends SherlockFragmentActivity implements OnQueryTextLi
 		args.putString("q", query);
 		
 		// get the fragment, and init the new search loader (using the search param)
-		EventListFragment frag = (EventListFragment) getCompatFragmentManager().findFragmentByTag("EVENT_LIST_FRAGMENT");
+		EventListFragment frag = (EventListFragment) getSupportFragmentManager().findFragmentByTag("EVENT_LIST_FRAGMENT");
 		frag.getLoaderManager().initLoader(EventListFragment.LOADERS.EVENTS, args, frag);
 		return true;
 	}
