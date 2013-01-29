473bcaeb236b6848770c01279dee982051bd05d3
Marc Meszaros <marc@snapable.com>
2013-01-28 22:42:16 -0500

    Initial work to start using ActionBarSherlock

--- app/src/ca/hashbrown/snapable/EventList.java
+++ app/src/ca/hashbrown/snapable/EventList.java
@@ -1,18 +1,19 @@
 package ca.hashbrown.snapable;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.widget.SearchView;
+import com.actionbarsherlock.widget.SearchView.OnQueryTextListener;
 
 import ca.hashbrown.snapable.fragments.EventListFragment;
-import ca.hashbrown.snapable.fragments.SearchBarFragment;
-import ca.hashbrown.snapable.fragments.SearchBarFragment.OnQueryTextListener;
 
-import android.os.Build;
 import android.os.Bundle;
 
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.SearchViewCompat;
-import android.support.v4.widget.SearchViewCompat.OnQueryTextListenerCompat;
 import android.util.Log;
 
 public class EventList extends SherlockFragmentActivity implements OnQueryTextListener {
@@ -20,54 +21,27 @@ public class EventList extends SherlockFragmentActivity implements OnQueryTextLi
 	private static final String TAG = "EventList";
 
 	public SearchViewCompat.OnQueryTextListenerCompat searchListener = null;
+	//ActionBar actionBar;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.activity_event_list);
+    	//actionBar = getSupportActionBar();
 
     	// create the fragments
-    	SearchBarFragment searchFragment = new SearchBarFragment();
     	EventListFragment eventListFragment = new EventListFragment();
 
     	// add the fragments
     	FragmentTransaction transaction = getCompatFragmentManager().beginTransaction();
-    	if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1) {
-    		transaction.add(R.id.activity_event_list__fragment_search_bar, searchFragment, "SEARCH_FRAGMENT");
-        }
     	transaction.add(R.id.activity_event_list__fragment_event_list, eventListFragment, "EVENT_LIST_FRAGMENT");
     	transaction.commit();
 	}
 
 	// used to pass back the search field listener back to the search view in the fragment
-	public OnQueryTextListenerCompat getOnQueryTextListenerCompat() {
-		return searchListener;
-	}
-
-	@Override
-	protected void onStart() {
-		super.onStart();
-		EventListFragment frag = (EventListFragment) getCompatFragmentManager().findFragmentByTag("EVENT_LIST_FRAGMENT");
-
-		// if the event list is there, we can create the listenet that will modify it
-		if (frag != null) {
-			
-			// create the search field listener that updates the list on submit
-			this.searchListener = new OnQueryTextListenerCompat() {
-	    		@Override
-	    		public boolean onQueryTextSubmit(String query) {
-	    			// build the search param
-	    			Bundle args = new Bundle(1);
-	    			args.putString("q", query);
-	    			
-	    			// get the fragment, and init the new search loader (using the search param)
-	    			EventListFragment frag = (EventListFragment) getCompatFragmentManager().findFragmentByTag("EVENT_LIST_FRAGMENT");
-	    			frag.getLoaderManager().initLoader(EventListFragment.LOADERS.EVENTS, args, frag);
-	    			return true;
-	    		}
-			};
-		}
-	}
+	//public OnQueryTextListenerCompat getOnQueryTextListenerCompat() {
+	//	return searchListener;
+	//}
 	
 	/**
 	 * 
@@ -76,5 +50,38 @@ public class EventList extends SherlockFragmentActivity implements OnQueryTextLi
 	private FragmentManager getCompatFragmentManager() {
 			return getSupportFragmentManager();
 	}
-	
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Create the search view
+        SearchView searchView = new SearchView(getSupportActionBar().getThemedContext());
+        searchView.setQueryHint("Event URL or Title");
+        searchView.setOnQueryTextListener(this);
+
+        menu.add("Search")
+            .setActionView(searchView)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+
+        return true;
+    }
+
+	@Override
+	public boolean onQueryTextSubmit(String query) {
+		Log.d(TAG, "search: " + query);
+		// build the search param
+		Bundle args = new Bundle(1);
+		args.putString("q", query);
+		
+		// get the fragment, and init the new search loader (using the search param)
+		EventListFragment frag = (EventListFragment) getCompatFragmentManager().findFragmentByTag("EVENT_LIST_FRAGMENT");
+		frag.getLoaderManager().initLoader(EventListFragment.LOADERS.EVENTS, args, frag);
+		return true;
+	}
+
+	@Override
+	public boolean onQueryTextChange(String newText) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
