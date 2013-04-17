11717e5827089118354b3d8fba4818fc9d666617
Marc Meszaros <marc@snapable.com>
2013-04-17 03:49:07 -0400

    New fancy transition loading of images

--- app/src/ca/hashbrown/snapable/provider/SnapCache.java
+++ app/src/ca/hashbrown/snapable/provider/SnapCache.java
@@ -1,32 +1,31 @@
 package ca.hashbrown.snapable.provider;
 
-import java.lang.ref.WeakReference;
-
-import org.codegist.crest.CRestException;
-
-import com.snapable.api.SnapClient;
-import com.snapable.api.resources.EventResource;
-import com.snapable.api.resources.PhotoResource;
-
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.AsyncTask;
 import android.support.v4.util.LruCache;
 import android.util.Log;
 import android.widget.ImageView;
+import com.snapable.api.SnapClient;
+import com.snapable.api.resources.EventResource;
+import com.snapable.api.resources.PhotoResource;
+import org.codegist.crest.CRestException;
+
+import java.lang.ref.WeakReference;
 
 /**
  * A class to help manage all the caching stuff.
  */
 public class SnapCache {
-	
+
 	private static final String TAG = "SnapCache";
-	
+
 	/**
 	 * A custom version of BitmapDrawable that wraps it with an AsuncTask to
-	 * load in the data. 
+	 * load in the data.
 	 */
 	public static class AsyncDrawable extends BitmapDrawable {
         private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
@@ -42,30 +41,39 @@ public class SnapCache {
     }
 
 	/**
-	 * Base AsyncTask used to load in images to an ImageView. 
+	 * Base AsyncTask used to load in images to an ImageView.
 	 */
 	public static abstract class BitmapWorkerTask extends AsyncTask<Long, Void, Bitmap> {
-		
+
 		protected long data = 0;
 		protected final WeakReference<ImageView> imageView;
-		
+
 		public BitmapWorkerTask(ImageView imageView) {
 			this.imageView = new WeakReference<ImageView>(imageView);
 		}
 
 		protected abstract Bitmap doInBackground(Long... params);
-		
+
 		@Override
 		protected void onPostExecute(Bitmap result) {
 			if (isCancelled()) {
 				result = null;
 			}
-			
+
 			if (this.imageView != null && result != null) {
 	            final ImageView imageView = this.imageView.get();
 	            final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
 	            if (this == bitmapWorkerTask && this.imageView != null) {
-	                imageView.setImageBitmap(result);
+                    // setup the drawables for the crossfade
+                    BitmapDrawable bitmapDrawable = new BitmapDrawable(imageView.getResources(), result);
+                    Drawable arrayDrawable[] = new Drawable[2];
+                    arrayDrawable[0] = imageView.getDrawable();
+                    arrayDrawable[1] = bitmapDrawable;
+                    // setup the transition
+                    TransitionDrawable transitionDrawable = new TransitionDrawable(arrayDrawable);
+                    transitionDrawable.setCrossFadeEnabled(true);
+                    imageView.setImageDrawable(transitionDrawable);
+                    transitionDrawable.startTransition(150);
 	            }
 	        }
 		}
@@ -80,7 +88,7 @@ public class SnapCache {
 	        }
 	        return null;
 	    }
-		
+
 		public static boolean cancelPotentialWork(long data, ImageView imageView) {
 	        final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
 
@@ -111,11 +119,11 @@ public class SnapCache {
 				return value.getRowBytes() * value.getHeight();
 			}
 		};
-		
+
 		public EventWorkerTask(ImageView imageView) {
 			super(imageView);
 		}
-		
+
 		@Override
 		protected Bitmap doInBackground(Long... params) {
 			this.data = params[0];
@@ -144,7 +152,7 @@ public class SnapCache {
 			final String imageKey = String.valueOf(id) + "_" + size;
 			return getBitmapFromCache(imageKey);
 		}
-		
+
 		public static void addBitmapToCache(String key, Bitmap bitmap) {
 			if (getBitmapFromCache(key) == null) {
 				mCache.put(key, bitmap);
@@ -156,12 +164,12 @@ public class SnapCache {
 		}
 
 	}
-	
+
 	/**
 	 * The photo caching and loading class.
 	 */
 	public static class PhotoWorkerTask extends BitmapWorkerTask {
-		
+
 		// 8MB of bitmap cache
 		private static LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(8 * 1024 * 1024) {
 			@Override
@@ -169,11 +177,11 @@ public class SnapCache {
 				return value.getRowBytes() * value.getHeight();
 			}
 		};
-		
+
 		public PhotoWorkerTask(ImageView imageView) {
 			super(imageView);
 		}
-		
+
 		@Override
 		protected Bitmap doInBackground(Long... params) {
 			this.data = params[0];
@@ -202,7 +210,7 @@ public class SnapCache {
 			final String imageKey = String.valueOf(id) + "_" + size;
 			return getBitmapFromCache(imageKey);
 		}
-		
+
 		public static void addBitmapToCache(String key, Bitmap bitmap) {
 			if (getBitmapFromCache(key) == null) {
 				mCache.put(key, bitmap);
