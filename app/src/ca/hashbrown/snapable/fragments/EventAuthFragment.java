f562efdeb664e5ad0b158df1201996da93787230
Marc Meszaros <marc@snapable.com>
2014-01-25 22:32:23 -0500

    Update the text for the dialog

--- app/src/ca/hashbrown/snapable/fragments/EventAuthFragment.java
+++ app/src/ca/hashbrown/snapable/fragments/EventAuthFragment.java
@@ -14,7 +14,6 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager.LayoutParams;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
