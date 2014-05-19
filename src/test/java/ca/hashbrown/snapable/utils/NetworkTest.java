f675eb7c1477b17f3f527a1b021f6ad261cfdced
Marc Meszaros <marc@snapable.com>
2014-05-19 14:10:00 -0400

    Update the unit test to actually test something

--- src/test/java/ca/hashbrown/snapable/utils/NetworkTest.java
+++ src/test/java/ca/hashbrown/snapable/utils/NetworkTest.java
@@ -2,18 +2,19 @@ package ca.hashbrown.snapable.utils;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertTrue;
 
-// Test class for MyActivity
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class NetworkTest {
 
     @Test
     public void connectionAvailable() throws Exception {
-        assertTrue(true);
+        Network net = new Network(Robolectric.application.getApplicationContext());
+        assertTrue(!net.isConnected());
     }
 }
