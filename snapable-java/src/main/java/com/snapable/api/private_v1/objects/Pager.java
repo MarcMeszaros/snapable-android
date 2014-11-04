a8af465867a5c60218258ba208faed4e36e872ed
Marc Meszaros <marc@snapable.com>
2014-11-03 23:05:44 -0800

    Nest the related objects

--- snapable-java/src/main/java/com/snapable/api/private_v1/objects/Pager.java
+++ snapable-java/src/main/java/com/snapable/api/private_v1/objects/Pager.java
@@ -2,17 +2,35 @@ package com.snapable.api.private_v1.objects;
 
 import com.snapable.util.ToStringHelper;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Pager<E> {
 
     public Meta meta;
-    public List<E> objects;
+    public ArrayList<E> objects;
 
     public String toString() {
         return ToStringHelper.getInstance(this)
                 .add("meta", meta)
                 .add("objects", objects)
                 .toString();
+    }
+
+    public class Meta {
+
+        public long limit;
+        public String next;
+        public long offset;
+        public String previous;
+        public long total_count;
+
+        public String toString() {
+            return ToStringHelper.getInstance(this)
+                    .add("limit", limit)
+                    .add("offset", offset)
+                    .add("total_count", total_count)
+                    .toString();
+        }
+
     }
 }
